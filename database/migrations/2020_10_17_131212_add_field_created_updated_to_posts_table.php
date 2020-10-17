08cb33b56f75dfdbbe00e074e93a507c801b5b98
Hendi <hendi@3trust.com>
2020-10-17 20:54:19 +0700

    menambahkan field created_by dan update_by di table posts

---
+++ database/migrations/2020_10_17_131212_add_field_created_updated_to_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldCreatedUpdatedToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->bigInteger('created_by')->nullable()->after('content'); 
+            $table->bigInteger('updated_by')->nullable()->after('created_by'); 
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('created_by');
+            $table->dropColumn('updated_by');
+            
+        });
+    }
+}
